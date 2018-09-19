101d75e7a5183662100b1bcada8e9b20a35f5145
Sid Prabhakaran <s2siddhu@gmail.com>
2018-09-18 19:47:23 -0700

    Renamed src files for consistency

---
+++ .vscode/cquery_cached_index/d@@vex prog@pros projects@kappatp/src@intake.cpp
@@ -0,0 +1,38 @@
+#include "main.h"
+
+using namespace pros;
+
+pros::Motor *intake;
+
+void initIntakeMotors() {
+  intake = new pros::Motor(MPORT_INTAKE, pros::E_MOTOR_GEARSET_18, 0);
+}
+
+void setIntake(int power) { intake->move(power); }
+
+void setIntakePosition(int position) { intake->move_absolute(position, 200); }
+
+void checkIntakeState() {
+  if (btnRising(E_CONTROLLER_DIGITAL_L1)) {
+    currIntakeState = intakeOpen;
+  }
+  if (btnRising(E_CONTROLLER_DIGITAL_L2)) {
+    currIntakeState = intakeClose;
+  }
+}
+
+void updateIntake() {
+
+  switch (currIntakeState) {
+  case intakeNotRunning:
+    break;
+
+  case intakeOpen:
+    setIntakePosition(50);
+    break;
+
+  case intakeClose:
+    setIntake(127);
+    break;
+  }
+}
